2ec54d102512f9992a73f7ec1eed98898bf91403
monroesummer <rustamasylgareev@gmail.com>
2017-05-21 18:36:29 +0300

    Added output in main page

--- src/AliMine/Controllers/HomeController.cs
+++ src/AliMine/Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using AliMine.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AliMine.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        AliMineContext db;
+        public HomeController(AliMineContext context)
         {
-            return View();
+            db = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Post> posts = db.Posts.Include(u => u.Category);
+
+            return View(await posts.AsNoTracking().ToListAsync());
+
         }
 
     }
